44e1d3f40b1888d9275cb2fa42940aa29245e45e
Sachinn-9700 <sachinn.bashetti@gmail.com>
2026-02-07 19:46:06 +0530

    uploading updated eks.file

--- terraform/eks-auth.tf
+++ terraform/eks-auth.tf
@@ -1,32 +1,7 @@
-resource "aws_eks_node_group" "worker_nodes" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "worker-nodes"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-
-  subnet_ids = aws_subnet.private[*].id
-
-  scaling_config {
-    desired_size = 2
-    min_size     = 1
-    max_size     = 3
-  }
-
-  instance_types = ["t3.medium"]
-  ami_type       = "AL2023_x86_64_STANDARD"
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_node_policies,
-    aws_eks_cluster.eks
-  ]
-}
-
-
 provider "kubernetes" {
-  host = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(
-    aws_eks_cluster.eks.certificate_authority[0].data
-  )
-  token = data.aws_eks_cluster_auth.eks.token
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 data "aws_eks_cluster_auth" "eks" {
@@ -44,20 +19,18 @@ resource "kubernetes_config_map_v1" "aws_auth" {
       {
         rolearn  = aws_iam_role.eks_node_role.arn
         username = "system:node:{{EC2PrivateDNSName}}"
-        groups = [
-          "system:bootstrappers",
-          "system:nodes"
-        ]
+        groups   = ["system:bootstrappers", "system:nodes"]
       },
       {
         rolearn  = aws_iam_role.jenkins_role.arn
         username = "jenkins"
-        groups = [
-          "system:masters"
-        ]
+        groups   = ["system:masters"] 
       }
     ])
   }
 
-  depends_on = [aws_eks_cluster.eks]
+  depends_on = [
+    aws_eks_node_group.worker_nodes,
+    aws_iam_role.jenkins_role
+  ]
 }
--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,44 +3,33 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks_cluster_role.arn
   version  = "1.33"
 
-
   vpc_config {
-    subnet_ids = concat(
-      aws_subnet.public[*].id,
-      aws_subnet.private[*].id
-    )
-
-    endpoint_public_access  = true
-    endpoint_private_access = false
+    subnet_ids = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy
+    aws_iam_role_policy_attachment.eks_cluster_policies
   ]
-  tags = {
-    Name = var.cluster_name
-  }
 }
 
-resource "aws_eks_node_group" "nodes" {
+resource "aws_eks_node_group" "worker_nodes" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "worker-nodes"
   node_role_arn   = aws_iam_role.eks_node_role.arn
 
   subnet_ids = aws_subnet.private[*].id
 
-
   scaling_config {
     desired_size = var.desired_capacity
-    max_size     = var.desired_capacity + 1
     min_size     = 1
+    max_size     = 3
   }
 
   instance_types = [var.node_instance_type]
   ami_type       = "AL2023_x86_64_STANDARD"
 
   depends_on = [
-    aws_eks_cluster.eks,
-    aws_iam_role.eks_node_role
+    aws_iam_role_policy_attachment.eks_node_policies,
+    aws_eks_cluster.eks
   ]
 }
